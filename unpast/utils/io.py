a3f622ea1a43e93800a3e62ca033dfafcae49344
Olga Zolotareva <olya.zolotareva@gmail.com>
2025-06-20 15:57:30 +0200

    read_bic_table upd - fails if the file is not found

--- unpast/utils/io.py
+++ unpast/utils/io.py
@@ -41,23 +41,18 @@ def read_bic_table(file_name: str, parse_metadata: bool = False) -> pd.DataFrame
     Example:
         biclusters, metadata = read_bic_table('biclusters.tsv', parse_metadata=True)
     """
-
-    if not os.path.exists(file_name):
-        return
+    
     biclusters = pd.read_csv(file_name, sep="\t", index_col=0, comment="#")
     if len(biclusters) == 0:
         return pd.DataFrame()
     else:
-        biclusters.loc[:, ["genes_up", "genes_down"]] = biclusters.loc[
-            :, ["genes_up", "genes_down"]
-        ].fillna("")
         biclusters["genes"] = biclusters["genes"].apply(
             lambda x: set([g for g in x.split(" ") if not g == ""])
         )
-        biclusters["genes_up"] = biclusters["genes_up"].apply(
+        biclusters["genes_up"] = biclusters["genes_up"].fillna("").apply(
             lambda x: set([g for g in x.split(" ") if not g == ""])
         )
-        biclusters["genes_down"] = biclusters["genes_down"].apply(
+        biclusters["genes_down"] = biclusters["genes_down"].fillna("").apply(
             lambda x: set([g for g in x.split(" ") if not g == ""])
         )
         biclusters["samples"] = biclusters["samples"].apply(lambda x: set(x.split(" ")))
