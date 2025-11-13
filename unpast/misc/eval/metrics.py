35ec49ce567ad046fdbb5a0bcdaf2f21cdabca5f
Alex Savchik <savsmail@gmail.com>
2025-11-13 13:28:27 +0100

    join _evaluate_overlaps functions

--- unpast/misc/eval/metrics.py
+++ unpast/misc/eval/metrics.py
@@ -69,21 +69,15 @@ def _process_class(
     """Process a single classification 'cl' and return (performance_value, best_match_stats_df).
 
     This extracts the logic previously inside the 'for cl in known_groups.keys()' loop.
-    Behavior is preserved exactly.
     """
     # denominator for weights
     N = 0
     for subt in known_groups_cl.keys():
         N += len(known_groups_cl[subt])
 
-    if performance_measure == "ARI":
-        pvals, is_enriched, performance = _evaluate_overlaps_ARI(
-            sample_clusters, known_groups_cl, all_samples
-        )
-    if performance_measure == "Jaccard":
-        pvals, is_enriched, performance = _evaluate_overlaps(
-            sample_clusters, known_groups_cl, all_samples
-        )
+    pvals, is_enriched, performance = _evaluate_overlaps(
+        sample_clusters, known_groups_cl, all_samples, method=performance_measure
+    )
 
     pvals = _adjust_pvals_df(pvals, adjust_pvals=adjust_pvals, pval_cutoff=pval_cutoff)
     best_match_stats = {}
@@ -157,80 +151,6 @@ def _filter_sample_clusters(sample_clusters_, min_n_samples, min_SNR, min_n_gene
     return sample_clusters
 
 
-def _evaluate_overlaps_ARI(biclusters, known_groups, all_elements):
-    # compute exact Fisher's p-values and Jaccard overlaps for samples
-    pvals = {}
-    is_enriched = {}
-    ARI = {}
-    N = len(all_elements)
-    all_elements_list = sorted(all_elements)
-    # sanity check - biclusters
-    for i in biclusters.index.values:
-        bic_members = biclusters.loc[i, "samples"]
-        if not bic_members.intersection(all_elements) == bic_members:
-            print(
-                "bicluster {} elements {} are not in 'all_elements'".format(
-                    i, " ".join(bic_members.difference(all_elements))
-                ),
-                file=sys.stderr,
-            )
-            bic_members = bic_members.intersection(all_elements)
-    # sanity check and sorting
-    group_names = list(known_groups.keys())
-    sorted_group_names = [group_names[0]]  # group names ordered by group size
-    for group in group_names:
-        group_members = known_groups[group]
-        if not group_members.intersection(all_elements) == group_members:
-            print(group, "elements are not in 'all_elements'", file=sys.stderr)
-            return
-
-        if group != group_names[0]:
-            for gn in range(len(sorted_group_names)):
-                if len(group_members) < len(known_groups[sorted_group_names[gn]]):
-                    sorted_group_names = (
-                        sorted_group_names[:gn] + [group] + sorted_group_names[gn:]
-                    )
-                    break
-                elif gn == len(sorted_group_names) - 1:
-                    sorted_group_names = [group] + sorted_group_names
-    # print(sorted_group_names)
-    for group in sorted_group_names:
-        group_members = known_groups[group]
-        pvals[group] = {}
-        is_enriched[group] = {}
-        ARI[group] = {}
-        # binary vector for target cluster
-        group_binary = np.zeros(len(all_elements_list))
-        for j in range(len(all_elements_list)):
-            if all_elements_list[j] in group_members:
-                group_binary[j] = 1
-        for i in biclusters.index.values:
-            bic = biclusters.loc[i, :]
-            bic_members = bic["samples"]
-            bic_binary = np.zeros(len(all_elements_list))
-            for j in range(len(all_elements_list)):
-                if all_elements_list[j] in bic_members:
-                    bic_binary[j] = 1
-            # calculate ARI for 2 binary vectors:
-            ARI[group][i] = adjusted_rand_score(group_binary, bic_binary)
-
-            # Fisher's exact test
-            shared = len(bic_members.intersection(group_members))
-            bic_only = len(bic_members.difference(group_members))
-            group_only = len(group_members.difference(bic_members))
-            union = shared + bic_only + group_only
-            pval = pvalue(shared, bic_only, group_only, N - union)
-            pvals[group][i] = pval.two_tail
-            is_enriched[group][i] = False
-            if pval.right_tail < pval.left_tail:
-                is_enriched[group][i] = True
-
-    pvals = pd.DataFrame.from_dict(pvals).loc[:, sorted_group_names]
-    is_enriched = pd.DataFrame.from_dict(is_enriched).loc[:, sorted_group_names]
-    ARI = pd.DataFrame.from_dict(ARI).loc[:, sorted_group_names]
-    return pvals, is_enriched, ARI
-
-
 def _apply_bh(df_pval, a=0.05):
     # applies BH procedure to each column of p-value table
     df_adj = {}
@@ -251,7 +171,6 @@ def _adjust_pvals_df(pvals, adjust_pvals, pval_cutoff=0.05):
     """
     if adjust_pvals == "B":
         # Bonferroni across each row/entry: multiply p-values by number of tests (columns)
-        # original behavior multiplied by pvals.shape[0] in-place; preserve that logic
         pvals = pvals * pvals.shape[0]
         pvals = pvals.applymap(lambda x: min(x, 1))
         return pvals
@@ -265,23 +184,29 @@ def _adjust_pvals_df(pvals, adjust_pvals, pval_cutoff=0.05):
         )
 
 
-def _evaluate_overlaps(biclusters, known_groups, all_elements, dimension="samples"):
-    # compute exact Fisher's p-values and Jaccard overlaps for samples
+def _evaluate_overlaps(biclusters, known_groups, all_elements, method, dimension="samples"):
+    # compute exact Fisher's p-values and Jaccard/ARI overlaps for samples
+    
+    assert method in ["Jaccard", "ARI"], f"Unsupported method: {method}, expected 'Jaccard' or 'ARI'."
     pvals = {}
     is_enriched = {}
-    jaccards = {}
+    metric_vals = {}
     N = len(all_elements)
+
+    if method == "ARI":
+        all_elements_list = sorted(all_elements)
+
     # sanity check - biclusters
     for i in biclusters.index.values:
         bic_members = biclusters.loc[i, dimension]
         if not bic_members.intersection(all_elements) == bic_members:
+            _diff_str = " ".join(bic_members.difference(all_elements))
             print(
-                "bicluster {} elements {} are not in 'all_elements'".format(
-                    i, " ".join(bic_members.difference(all_elements))
-                ),
+                f"bicluster {i} elements {_diff_str} are not in 'all_elements'",
                 file=sys.stderr,
             )
             bic_members = bic_members.intersection(all_elements)
+    
     # sanity check and sorting
     group_names = list(known_groups.keys())
     sorted_group_names = [group_names[0]]  # group names ordered by group size
@@ -300,21 +225,44 @@ def _evaluate_overlaps(biclusters, known_groups, all_elements, dimension="sample
                     break
                 elif gn == len(sorted_group_names) - 1:
                     sorted_group_names = [group] + sorted_group_names
+    
     # print(sorted_group_names)
     for group in sorted_group_names:
         group_members = known_groups[group]
         pvals[group] = {}
         is_enriched[group] = {}
-        jaccards[group] = {}
+        metric_vals[group] = {}
+
+        if method == "ARI":
+            # binary vector for target cluster
+            group_binary = np.zeros(len(all_elements_list))
+            for j in range(len(all_elements_list)):
+                if all_elements_list[j] in group_members:
+                    group_binary[j] = 1
+
         for i in biclusters.index.values:
             bic = biclusters.loc[i, :]
             bic_members = bic[dimension]
-            shared = len(bic_members.intersection(group_members))
+           
+            if method == "ARI":
+                bic_binary = np.zeros(len(all_elements_list))
+                for j in range(len(all_elements_list)):
+                    if all_elements_list[j] in bic_members:
+                        bic_binary[j] = 1
+                # calculate ARI for 2 binary vectors:
+                metric_vals[group][i] = adjusted_rand_score(group_binary, bic_binary)
+
+            # Fisher's exact test
+            shared = len(bic_members.intersection(group_members))   
             bic_only = len(bic_members.difference(group_members))
             group_only = len(group_members.difference(bic_members))
             union = shared + bic_only + group_only
             pval = pvalue(shared, bic_only, group_only, N - union)
-            """pvals[group][i] = 1 
+
+
+            # some commented out code in the Jaccard version above for under-representation handling
+            """
+            pvals[group][i] = 1 
             if pval.right_tail < pval.left_tail:
                 pvals[group][i] = pval.right_tail
                 is_enriched[group][i] = True
@@ -324,17 +272,18 @@ def _evaluate_overlaps(biclusters, known_groups, all_elements, dimension="sample
                 is_enriched[group][i] = False
                 bic_members = all_elements.difference(bic_members)
                 shared = len(bic_members.intersection(group_members))
-                union = len(bic_members.union(group_members))"""
+                union = len(bic_members.union(group_members))
+            """
+            
             pvals[group][i] = pval.two_tail
             is_enriched[group][i] = False
             if pval.right_tail < pval.left_tail:
                 is_enriched[group][i] = True
 
-            jaccards[group][i] = shared / union
-
-        # print(group,jaccards[group])
+            if method == "Jaccard":
+                metric_vals[group][i] = shared / union
 
     pvals = pd.DataFrame.from_dict(pvals).loc[:, sorted_group_names]
     is_enriched = pd.DataFrame.from_dict(is_enriched).loc[:, sorted_group_names]
-    jaccards = pd.DataFrame.from_dict(jaccards).loc[:, sorted_group_names]
-    return pvals, is_enriched, jaccards
+    metric_vals = pd.DataFrame.from_dict(metric_vals).loc[:, sorted_group_names]
+    return pvals, is_enriched, metric_vals
